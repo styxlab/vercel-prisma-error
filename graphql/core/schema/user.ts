c6150d82c30c18bd10e210aa18e8820bf6fa4eac
jj <jj>
2021-05-11 15:33:01 +0200

    rm role

--- graphql/core/schema/user.ts
+++ graphql/core/schema/user.ts
@@ -1,30 +1,30 @@
-import { objectType, extendType } from '@nexus/schema'
+import { objectType, extendType } from "@nexus/schema";
 
 const User = objectType({
-  name: 'User',
+  name: "User",
   definition(t) {
-    t.model.id()
-    t.model.name()
-    t.model.email()
-    t.model.image()
-    t.model.role()
-    t.model.emailVerified()
-    t.model.createdAt()
+    t.model.id();
+    t.model.name();
+    t.model.email();
+    t.model.image();
+    t.model.emailVerified();
+    t.model.createdAt();
   },
-})
+});
 
 const Query = extendType({
-  type: 'Query',
+  type: "Query",
   definition(t) {
-    t.list.field('allUsers', {
-      type: 'User',
+    t.list.field("allUsers", {
+      type: "User",
       resolve: async (_root, args, ctx) => {
-        if (!(await ctx.prisma.user.findFirst({ where: { id: ctx.user.id, role: 'ADMIN' } }))) return null
-        return ctx.prisma.user.findMany()
+        if (!(await ctx.prisma.user.findFirst({ where: { id: ctx.user.id } })))
+          return null;
+        return ctx.prisma.user.findMany();
       },
-    })
-    t.crud.user()
+    });
+    t.crud.user();
   },
-})
+});
 
-export default [User, Query]
+export default [User, Query];
